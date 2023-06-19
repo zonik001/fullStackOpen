de02d6299daeb9050cdc2f478720a54cfe956568
hubohan <hubohan@sinosoft.com.cn>
2023-06-19 18:01:28 +0800

    part4

--- psrt4/cuorseInfo/d.密钥认证/note_backend/controllers/notes.js
+++ psrt4/cuorseInfo/d.密钥认证/note_backend/controllers/notes.js
@@ -1,6 +1,7 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note
@@ -22,10 +23,23 @@ notesRouter.get('/:id', (request, response, next) => {
 })
 
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  // const user = await User.findById(body.userId)
 
   const note = new Note({
     content: body.content,
